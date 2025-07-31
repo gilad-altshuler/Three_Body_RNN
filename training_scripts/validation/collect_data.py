084e6aa5d15b8c3cc1ddd2914d2e1e3953b4ae64
gilad-altshuler <giladalt1@gmail.com>
2025-07-31 16:43:31 +0300

    fix collect bugs

--- training_scripts/validation/collect_data.py
+++ training_scripts/validation/collect_data.py
@@ -8,11 +8,12 @@ RUN = 1
 RUN_DIR = ROOT.parent / "runs" / "low_rank_procedures" / TASK
 DATA_DIR = ROOT / "data" / "validation"
 methods = ["tca", "tt", "slice_tca", "lint"]
+runs = 30
 
 for m in methods:
     data = []
-    for i in range(1, 33):
-        path = RUN_DIR / f"{i:03d}/{m}.npy"
+    for run in range(1, runs+1):
+        path = RUN_DIR / f"{run:03d}/{m}.npy"
         if os.path.exists(path):
             arr = np.load(path)
             if arr.shape == (6,):
@@ -23,11 +24,11 @@ for m in methods:
             print(f"❌ Missing: {path}")
 
     stacked = np.stack(data)
-    print("✅ Loaded shape:", stacked.shape)
+    print(f"✅ {m} loaded shape:", stacked.shape)
     np.save(DATA_DIR / f"all_{m}.npy", stacked)
 
-    print("saving teacher tbrnn model..")
-    torch.save(torch.load(RUN_DIR / f"{RUN:03d}" / "teacher_model.pth", weights_only=True), DATA_DIR / "teacher_model.pth")
+print("saving teacher tbrnn model..")
+torch.save(torch.load(RUN_DIR / f"{RUN:03d}" / "teacher_model.pth", weights_only=True), DATA_DIR / "teacher_model.pth")
 
-    print("Done. All data saved to:", DATA_DIR)
+print("Done. All data saved to:", DATA_DIR)
 
