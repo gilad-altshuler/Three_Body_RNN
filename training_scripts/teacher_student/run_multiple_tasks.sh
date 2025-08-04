c4a3936ca677f589f72a4bc56cbf202a6b8a1f9d
gilad-altshuler <giladalt1@gmail.com>
2025-08-04 09:33:22 +0300

    changing path to root based

--- training_scripts/teacher_student/run_multiple_tasks.sh
+++ training_scripts/teacher_student/run_multiple_tasks.sh
@@ -5,11 +5,11 @@ ROOT="$(dirname "$(dirname "$(dirname "$SCRIPT_PATH")")")"
 
 # Run flip-flop jobs
 echo "Starting flip-flop jobs..."
-bash training_scripts/teacher_student/run_multiple_flipflop.sh
+bash "$ROOT/training_scripts/teacher_student/run_multiple_flipflop.sh"
 
 # After flip-flop jobs finish, run sine jobs
 echo "Starting sine jobs..."
-bash training_scripts/teacher_student/run_multiple_sin.sh
+bash "$ROOT/training_scripts/teacher_student/run_multiple_sin.sh"
 
 echo "All jobs completed."
 
