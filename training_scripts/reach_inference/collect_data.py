f5afb561341a60d3523b6c50c949faf6c53aa43f
gilad-altshuler <giladalt1@gmail.com>
2025-08-24 00:15:58 +0300

    collect reach data

---
+++ training_scripts/reach_inference/collect_data.py
@@ -0,0 +1,32 @@
+import os
+from pathlib import Path
+import numpy as np
+import h5py
+
+
+ROOT = Path(__file__).absolute().parent.parent.parent
+RUN_DIR = ROOT.parent / "ext" / "runs" / "reach_conditioning"
+DATA_DIR = ROOT / "data" / "mante_inference"
+CONFIGS=(
+  "r_5_rnn",
+  "r_6_rnn",
+  "r_4_r_1_hornn",
+  "r_5_r_1_hornn",
+  "r_6_r_1_hornn",
+)
+RUNS = 30
+
+if not os.path.isdir(RUN_DIR):
+    print(f"Run directory {RUN_DIR} does not exist. Please run the training script first.")
+    exit(1)
+print("Configs:")
+for cfg in CONFIGS: print(cfg)
+
+r2s = {cfg: np.array([h5py.File(RUN_DIR / f"{cfg}/{run:03d}/checkpoints/best/metrics.h5", "r")["vel R2"][()] for run in range(1,RUNS)]) for cfg in CONFIGS}
+
+if not os.path.isdir(DATA_DIR):
+    DATA_DIR.mkdir(parents=True)
+
+np.save(DATA_DIR / "r2s.npy", r2s)
+
+print("Done. All data saved to:", DATA_DIR)
